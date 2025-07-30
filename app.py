1c0af6eefc313a18b5fad711c14f5802a1a0ea16
wandystoa-infinity <stoainfinity42@gmail.com>
2025-07-31 00:28:56 +0700

    Fix Procfile and app config

--- app.py
+++ app.py
@@ -108,6 +108,4 @@ def upload():
 
 # --- Jalankan Aplikasi ---
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
